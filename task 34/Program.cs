ab5a352e84c00b11a3d731b1038b43a9a7b9c80a
Maria <mariya-ivanova@list.ru>
2024-06-01 22:24:55 +0200

    task34 was solved

---
+++ task 34/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int CountEvenNumbers(int[] array)
+{
+   int counter = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] % 2 == 0)
+      {
+         counter++;
+      } 
+   }
+   return counter;
+}
+
+int[] GetRandomArray(int size, int start, int end) 
+{
+  int[] array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(start, end+1);
+  }
+  return array;  
+}
+
+int[] array = GetRandomArray(20, 100, 999);
+Console.WriteLine("[" + string.Join(",", array) + "]");
+Console.WriteLine($"количество четных чисел в массиве равно: {CountEvenNumbers(array)}");
